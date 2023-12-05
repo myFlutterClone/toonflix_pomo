d67430eb95e369650b6d3cb9747a837605678b2e
앙구리 <man3801@gmail.com>
2023-12-05 13:58:26 +0900

    fister commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix_pomo/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xff232b55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4Eddb),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
